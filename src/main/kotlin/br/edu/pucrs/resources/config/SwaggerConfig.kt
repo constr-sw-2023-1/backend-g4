c049b65147ce0ea62474828490046ee749dae3f9
Leonardo Berlatto <36887738+LeonardoBerlatto@users.noreply.github.com>
2023-05-20 18:29:36 -0300

    Feat/spring security (#9)
    * Adding spring security
    
    * Adding spring security config
    
    * Adding swagger config
    
    * Adding keycloak basic data
    
    * Adding keycloak container

---
+++ src/main/kotlin/br/edu/pucrs/resources/config/SwaggerConfig.kt
@@ -0,0 +1,39 @@
+package br.edu.pucrs.resources.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+private const val SECURITY_SCHEME_NAME = "bearerAuth"
+private const val BEARER_FORMAT = "JWT"
+private const val SECURITY_SCHEME_TYPE = "bearer"
+
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun api(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Resources API - Group 4")
+                    .description("Resources API documentation")
+                    .version("v0.0.1")
+            )
+            .addSecurityItem(SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        SECURITY_SCHEME_NAME, SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme(SECURITY_SCHEME_TYPE)
+                            .bearerFormat(BEARER_FORMAT)
+                    )
+            )
+    }
+}
